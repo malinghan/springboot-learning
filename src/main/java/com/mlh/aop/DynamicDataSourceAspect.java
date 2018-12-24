db79c47d150d00d4ae1fbf881c163714f41e5a05
马凌汉 <malinghan@qiyeyoudao.com>
2018-12-24 12:00:02 +0800

    submit

--- src/main/java/com/mlh/aop/DynamicDataSourceAspect.java
+++ src/main/java/com/mlh/aop/DynamicDataSourceAspect.java
@@ -16,12 +16,20 @@ import org.springframework.stereotype.Component;
  * @author: linghan.ma
  * @DATE: 2018/9/26
  * @description: 动态数据源通知
- * @see:  https://www.cnblogs.com/foreveravalon/p/8653832.html
+ * @see: https://www.cnblogs.com/foreveravalon/p/8653832.html
+ * @EnableAspectJAutoProxy： 表示开启AOP代理自动配置，如果配@EnableAspectJAutoProxy表示使用cglib进行代理对象的生成；设置@EnableAspectJAutoProxy(exposeProxy=true)表示通过aop框架暴露该代理对象，aopContext能够访问.
+ * <p>
+ * 从@EnableAspectJAutoProxy的定义可以看得出，它引入AspectJAutoProxyRegister.class对象，该对象是基于注解@EnableAspectJAutoProxy注册一个AnnotationAwareAspectJAutoProxyCreator，该对象通过调用AopConfigUtils.registerAspectJAnnotationAutoProxyCreatorIfNecessary(registry);注册一个aop代理对象生成器。
+ * ---------------------
+ * 作者：阿里-橙鹰-潘民兰
+ * 来源：CSDN
+ * 原文：https://blog.csdn.net/pml18710973036/article/details/61654277
+ * 版权声明：本文为博主原创文章，转载请附上博文链接！
  */
 @Component
 @Order(-1) // 保证在 @Transactional之前执行
 @Aspect
-@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableAspectJAutoProxy(proxyTargetClass = true) //使用CGLIB代理
 public class DynamicDataSourceAspect {
 
     private static final Logger logger = LogManager.getLogger(DynamicDataSourceAspect.class);
@@ -30,18 +38,19 @@ public class DynamicDataSourceAspect {
     @Before("@annotation(targetDataSource)")
     public void changeDataSource(JoinPoint joinPoint, TargetDataSource targetDataSource) {
         String dbid = targetDataSource.name();
-        logger.debug("使用数据源：" + dbid+ joinPoint.getSignature());
+        logger.debug("使用数据源：" + dbid + joinPoint.getSignature());
         DynamicDataSourceContextHolder.setDatabaseType(dbid);
     }
 
     /**
      * 销毁数据源  在所有的方法执行执行完毕后
+     *
      * @param joinPoint
      * @param targetDataSource
      */
     @After("@annotation(targetDataSource)")
     public void clearDataSource(JoinPoint joinPoint, TargetDataSource targetDataSource) {
-        logger.debug("清除数据源 " + targetDataSource.name() + " !",joinPoint.getSignature());
+        logger.debug("清除数据源 " + targetDataSource.name() + " !", joinPoint.getSignature());
         DynamicDataSourceContextHolder.clearDataSoureType();
     }
 }
